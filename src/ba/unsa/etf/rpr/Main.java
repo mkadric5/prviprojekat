239833112ebe3f56eeb4b843e627780a16ce8bf9
mkadric5 <mkadric5@etf.unsa.ba>
2020-10-24 23:59:58 +0100

    Dodan ispis dodatne poruke o Javi

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Vozdra raja, ovo je prvi Java projekat");
+        System.out.println("Java je dobar programski jezik");
     }
 }
